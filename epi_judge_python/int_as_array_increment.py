b0a8717d7ecae00692d5065e52262d569bf7656a
Kyle Balisnomo <kylebalisnomo@gmail.com>
2024-03-20 17:47:53 -0600

    Add solution to 5.2

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -2,10 +2,22 @@ from typing import List
 
 from test_framework import generic_test
 
+# ex [1, 2, 9]
+# ex [1, 3, 0]
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i-1] += 1
+        print(A)
+    else:
+        if A[0] == 10:
+            A[0] = 1
+            A.append(0)
+    return A
 
 
 if __name__ == '__main__':
